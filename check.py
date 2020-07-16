ae4cac7c744605daba1e6f52ced16e5dff9d8404
mrfrog1955 <68384406+mrfrog1955@users.noreply.github.com>
2020-07-16 13:25:54 +0000

    Update check.py

--- check.py
+++ check.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 import os
 
-
-domains = ['www.allenservice.ga','www.castmart.ga', 'stngpetty.ga', 'ggautosrep.ga', 'google.com']
-
+#add your domains in here
+domains = ['']
+#this will download the pages in question and place them in your current directory
 for i in domains:
 	response = os.system("ping -c 1 " + i + "> /dev/null 2>&1")
 	if response == 0:
